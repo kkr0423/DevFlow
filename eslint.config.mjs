ef555aadfd56c8cf57d1ef0f2064ccfd2a2cf033
Ogasawara <kakeru19870423@gmail.com>
2026-05-04 13:44:05 +0900

    set up testing enviroment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,6 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,7 +10,10 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "components/ui/**",
   ]),
+  ...nextVitals,
+  ...nextTs,
 ]);
 
 export default eslintConfig;
